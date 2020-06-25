fd2131ca50159c6538bcec45f3f62aa6c640b5cd
KUROIWA, Keijiro <kuronicle@gmail.com>
2020-06-25 13:55:27 +0900

    First commit. These ware generated by Spring Initializr.

---
+++ src/test/java/net/kuronicle/todo/api/TodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kuronicle.todo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
